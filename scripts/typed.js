9941eaa66912ebb1af2fe1d3248beba41b611b13
Phillips <lennonphillips1@KALIDEAMON>
2024-07-08 10:20:18 -0400

    nuevo portafolio

---
+++ scripts/typed.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#typed-text', {
+  strings: [' Frontend Developer'],
+  typeSpeed: 50,
+  backSpeed: 25,
+  loop: true,
+  loopCount: Infinity,
+  backDelay: 3000,
+  // startDelay: 0  // Opcional: elimina el retraso inicial si lo deseas
+});
